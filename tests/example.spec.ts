6cfba1026aed219a2fab55f282f0fb2ac851e700
alejandro-almeida-improving <alejandro.almeida@improving.com>
2025-11-20 12:59:20 -0700

    Update test configurations and modify example test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('home page displays the getting started text', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Verify that the page contains the expected text
+  await expect(page.getByText('To get started, edit the page.tsx file.')).toBeVisible();
 });
